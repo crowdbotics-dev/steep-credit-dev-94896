bf68d10ce2fcf6f1ab86cf61b83b657f90338e41
Crowdbotics <engineering@crowdbotics.com>
2023-06-26 23:15:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-credit-dev-94896",
+  url: "https://steep-credit-dev-94896.botics.co",
+  api: "https://steep-credit-dev-94896.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
